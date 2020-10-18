c878fdb6c5aee79cea6d436d65e7c85c28c56534
Aland Mariwan <59203362+amariwan@users.noreply.github.com>
2020-10-18 16:08:19 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/main.css'
